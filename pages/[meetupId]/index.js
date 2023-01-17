60a53d69acbfb3a3b49854d9bc8d5c4398eedba6
andrew2263 <andrew2263@gmail.com>
2023-01-17 18:46:21 +0200

    meetup_detail

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/d/dd/Wien_-_Stephansdom_%281%29.JPG'
+      title='First Meetup'
+      address='Some Street, 5, Vienna'
+      description='This is a first meetup'
+    />
+  );
+}
+
+export default MeetupDetails;
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,16 @@
+// our-domain.com/new-meetup
+
+import Layout from '../../components/layout/Layout';
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {
+    console.log(enteredMeetupData);
+  }
+
+  return (
+    <NewMeetupForm onAddMeetup={ addMeetupHandler } />
+  )
+};
+
+export default NewMeetupPage;
